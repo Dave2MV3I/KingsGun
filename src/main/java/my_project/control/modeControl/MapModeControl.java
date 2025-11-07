ffa69e7d890634d0bea7a80033b6e233a2d5f047
Tom0le <schutom@kkg-do.eu>
2025-11-07 10:52:05 +0100

    Village in Progress

--- src/main/java/my_project/control/modeControl/MapModeControl.java
+++ src/main/java/my_project/control/modeControl/MapModeControl.java
@@ -1,6 +1,16 @@
 package my_project.control.modeControl;
 
+import KAGO_framework.model.abitur.datenstrukturen.List;
+import my_project.model.Village;
+
 public class MapModeControl extends ModeControl {
+    private List<Village> villages;
+
+    public MapModeControl() {
+        VillageModeControl villageController = (VillageModeControl) mainController.getModeControls().get("village");
+        villages = villageController.getVillages();
+    }
+
     @Override
     protected void deactivate() {
     }
