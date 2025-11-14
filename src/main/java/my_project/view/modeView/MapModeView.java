cdb1565bad88230fa4ba79b921f2c6712e5a68e4
Tom0le <schutom@kkg-do.eu>
2025-11-14 20:32:52 +0100

    Street

--- src/main/java/my_project/view/modeView/MapModeView.java
+++ src/main/java/my_project/view/modeView/MapModeView.java
@@ -19,26 +19,41 @@ public class MapModeView extends ModeView {
     public void setAmountVillages(int amountVillages) {
         System.out.println("Amount of villages: " + amountVillages);
         this.amountVillages = amountVillages;
-        villagePos = new int[amountVillages][2];
-        for(int i = 0; i < amountVillages; i++){
-            villagePos[i][0] = Config.WINDOW_WIDTH/2 + (int)(Math.random()*100)-50;
-            villagePos[i][1] = (int) (((Config.WINDOW_HEIGHT / 1.2) - (Config.WINDOW_HEIGHT / 6)) / (amountVillages+1)) * (i+1) + Config.WINDOW_HEIGHT/6;
+        villagePos = new int[amountVillages+2][2];
+        for(int i = 0; i < amountVillages+2; i++){
+            if(i == 0 || i == amountVillages+1)  {
+                villagePos[i][0] = Config.WINDOW_WIDTH / 2;
+            }else {
+                villagePos[i][0] = Config.WINDOW_WIDTH / 2 + (int) (Math.random() * 150) - 75;
+            }
+            villagePos[i][1] = (int) (((Config.WINDOW_HEIGHT / 1.2) - (Config.WINDOW_HEIGHT / 6)) / (amountVillages+1)) * (i) + Config.WINDOW_HEIGHT/6;
         }
     }
 
     @Override
     public void draw(DrawTool drawTool) {
+
+        for(int i = 0; i < villagePos.length - 1; i++) {
+            drawTool.setCurrentColor(new Color(24, 20, 37));
+            drawTool.setLineWidth(10);
+            drawTool.drawLine(villagePos[i][0], villagePos[i][1], villagePos[i+1][0], villagePos[i+1][1]);
+            drawTool.setCurrentColor(new Color(228, 166, 114));
+            drawTool.setLineWidth(8);
+            drawTool.drawLine(villagePos[i][0], villagePos[i][1], villagePos[i+1][0], villagePos[i+1][1]);
+        }
+
         drawTool.setCurrentColor(new Color(255, 0, 0));
         drawTool.drawFilledCircle(Config.WINDOW_WIDTH/2, Config.WINDOW_HEIGHT/6,10);
-        drawTool.setCurrentColor(new Color(44, 44, 44));
+        drawTool.setCurrentColor(new Color(38, 43, 68));
         drawTool.drawFilledRectangle(Config.WINDOW_WIDTH/2-50, Config.WINDOW_HEIGHT/10,100,40);
         drawTool.drawFilledRectangle(Config.WINDOW_WIDTH/2-50, Config.WINDOW_HEIGHT/10-40,35,40);
         drawTool.drawFilledRectangle(Config.WINDOW_WIDTH/2+15, Config.WINDOW_HEIGHT/10-40,35,40);
 
         drawTool.setCurrentColor(new Color(54, 182, 11));
-        for(int i = 0; i < amountVillages; i++) {
+        for(int i = 1; i < amountVillages+1; i++) {
             drawTool.drawFilledCircle(villagePos[i][0], villagePos[i][1], 10);
         }
+
 
         drawTool.setCurrentColor(new Color(13, 152, 241));
         drawTool.drawFilledCircle(Config.WINDOW_WIDTH/2, Config.WINDOW_HEIGHT/1.2,10);
