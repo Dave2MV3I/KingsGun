431663724abace2143edfc71bbbb4238f9f2cdc4
badamykh <badamykh@kkg-do.eu>
2025-11-17 09:29:39 +0100

    Coding TravelModeView started (Road)

--- src/main/java/my_project/view/modeView/TravelModeView.java
+++ src/main/java/my_project/view/modeView/TravelModeView.java
@@ -1,10 +1,25 @@
 package my_project.view.modeView;
 
+import KAGO_framework.view.DrawTool;
+import my_project.Config;
 import my_project.control.modeControl.ModeControl;
 import my_project.control.modeControl.TravelModeControl;
+import my_project.model.Graphics.Texture;
 
 public class TravelModeView extends ModeView {
+    private Texture roadTexture;
     public TravelModeView(TravelModeControl modeControl) {
         super(modeControl);
+        roadTexture = new Texture("road.png");
+    }
+    @Override
+    public void draw(DrawTool drawTool) {
+        for (int i = 0; i < 100; i++) {
+            roadTexture.drawToWidth(drawTool, 0, 0 - i * roadTexture.getHeightRelativeToWidth(Config.WINDOW_WIDTH), Config.WINDOW_WIDTH);
+        }
+    }
+
+    public void update(double dt){
+
     }
 }
