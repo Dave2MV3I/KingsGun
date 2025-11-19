305353a1aaa5e4adf98a5accb871e564619de975
badamykh <badamykh@kkg-do.eu>
2025-11-19 13:15:30 +0100

    Coding TravelModeView started (Road) - fixed follow method in MainView - Created and instantiated Carriage - Implemented Automatic drawing of textures in the travel mode - added getScaleRelativeToWidth() in Texture.java - added textures of road & carriage

--- src/main/java/my_project/view/modeView/TravelModeView.java
+++ src/main/java/my_project/view/modeView/TravelModeView.java
@@ -5,21 +5,28 @@ import my_project.Config;
 import my_project.control.modeControl.ModeControl;
 import my_project.control.modeControl.TravelModeControl;
 import my_project.model.Graphics.Texture;
+import my_project.model.TravelMode.Carriage;
+import my_project.view.MainView;
 
 public class TravelModeView extends ModeView {
     private Texture roadTexture;
+    public double yPos;
+    private double scale;
+    private Carriage carriage;
     public TravelModeView(TravelModeControl modeControl) {
+
         super(modeControl);
-        roadTexture = new Texture("road.png");
+        roadTexture = new Texture("road_3.png");
+        carriage = ((TravelModeControl)modeControl).getCarriage();
+        yPos = 600;
+        MainView.setScale(roadTexture.getScaleRelativeToWidth(Config.WINDOW_WIDTH));
     }
     @Override
     public void draw(DrawTool drawTool) {
         for (int i = 0; i < 100; i++) {
-            roadTexture.drawToWidth(drawTool, 0, 0 - i * roadTexture.getHeightRelativeToWidth(Config.WINDOW_WIDTH), Config.WINDOW_WIDTH);
+            roadTexture.autoDraw(drawTool, 0, i * 16, 256);
         }
+        carriage.draw(drawTool);
     }
 
-    public void update(double dt){
-
-    }
 }
