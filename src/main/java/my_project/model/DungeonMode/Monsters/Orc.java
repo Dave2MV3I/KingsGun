f10a27f32877897093a7c5886eb4cdb9e97f6c2b
Dave2MV3I <dagl777-github@yahoo.com>
2025-12-05 11:25:57 +0100

    enum weiter

--- src/main/java/my_project/model/DungeonMode/Monsters/Orc.java
+++ src/main/java/my_project/model/DungeonMode/Monsters/Orc.java
@@ -1,4 +1,9 @@
 package my_project.model.DungeonMode.Monsters;
 
+import static my_project.model.DungeonMode.Tasks.Attack.*;
+
 public class Orc extends Monster{
+    public Orc(){
+        super({WEIGHT_ATTACK});
+    }
 }
