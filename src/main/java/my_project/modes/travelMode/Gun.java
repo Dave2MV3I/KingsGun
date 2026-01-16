626dfbbaf45a705290d0bed6c34c93df5625c508
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2026-01-16 22:03:32 +0100

    Saved Aammo Inventory and current Village in Player, to reset after death and remain between modes. current Village still needs to be implemented by tomole

--- src/main/java/my_project/modes/travelMode/Gun.java
+++ src/main/java/my_project/modes/travelMode/Gun.java
@@ -1,4 +1,24 @@
 package my_project.modes.travelMode;
 
+
+import my_project.view.InputManager;
+import my_project.view.Mouse;
+
+import java.awt.event.KeyEvent;
+
 public class Gun {
+    private Carriage carriage;
+    public Gun(Carriage carriage) {
+        this.carriage = carriage;
+    }
+    public void update(double dt) {
+        if (InputManager.isPressed(KeyEvent.VK_SPACE) || Mouse.isDown(1)) {
+            if (carriage.getAmmo().top() != null) {
+                System.out.println(carriage.getAmmo().top().getClass().getSimpleName());
+                carriage.getAmmo().pop();
+            }else {
+                System.out.println("blyat");
+            }
+        }
+    }
 }
