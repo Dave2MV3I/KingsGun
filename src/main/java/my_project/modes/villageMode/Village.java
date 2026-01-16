0850483d9ff516d122c941560804b8a258f3924c
Tom0le <schutom@kkg-do.eu>
2026-01-16 11:13:11 +0100

    Village <--> Map fix

--- src/main/java/my_project/modes/villageMode/Village.java
+++ src/main/java/my_project/modes/villageMode/Village.java
@@ -2,12 +2,17 @@ package my_project.modes.villageMode;
 
 public class Village {
     private String dungeonMap = "src/main/resources/graphic/testImage.png";
+    private int mapX;
+    private int mapY;
 
-    public Village() {
-
+    public Village(int x, int y) {
+        mapX = x;
+        mapY = y;
     }
 
     public String getVillageMap() {
         return dungeonMap;
     }
+    public int getMapX() {return mapX;}
+    public int getMapY() {return mapY;}
 }
